32af9a8ddf435d63ab1fb97d965ac1e3ac80b1eb
YanaSmirnova <smirnova.yana@gmail.com>
2015-02-09 23:49:07 +1100

    Initial commit

---
+++ app/src/main/java/yanasmirnova/com/blogreader/MainListActivity.java
@@ -0,0 +1,14 @@
+package yanasmirnova.com.blogreader;
+
+import android.app.ListActivity;
+import android.os.Bundle;
+
+public class MainListActivity extends ListActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_list);
+    }
+
+}
