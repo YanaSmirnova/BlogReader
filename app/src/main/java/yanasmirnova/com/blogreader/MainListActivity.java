3f5b92d32ed1c24f71a1cdee61b23c3b2b309379
YanaSmirnova <smirnova.yana@gmail.com>
2015-02-10 02:26:14 +1100

    Set list adapter, reset array through string array in xml

--- app/src/main/java/yanasmirnova/com/blogreader/MainListActivity.java
+++ app/src/main/java/yanasmirnova/com/blogreader/MainListActivity.java
@@ -1,28 +1,24 @@
 package yanasmirnova.com.blogreader;
 
 import android.app.ListActivity;
+import android.content.res.Resources;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 
 public class MainListActivity extends ListActivity {
 
-    protected String[] mAndroidNames = {
-            "Android beta",
-            "Android 1.0",
-            "Android 1.1",
-            "Cupcake",
-            "Donut",
-            "Eclair",
-            "Froyo",
-            "Gingerbread",
-            "Honeycomb",
-            "Ice Cream Sandwich",
-            "Jelly Bean"
-    };
+    protected String[] mAndroidNames;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_list);
+
+        Resources resources = getResources();
+        mAndroidNames = resources.getStringArray(R.array.android_names);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, mAndroidNames);
+        setListAdapter(adapter);
     }
 
 }
