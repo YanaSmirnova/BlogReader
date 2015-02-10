f95b4895f1866bb6105cda9dabe80c30ba7be5e3
YanaSmirnova <smirnova.yana@gmail.com>
2015-02-10 15:10:01 +1100

    Created an inner AsyncTask class getting data from the Treehouse blog

--- app/src/main/java/yanasmirnova/com/blogreader/MainListActivity.java
+++ app/src/main/java/yanasmirnova/com/blogreader/MainListActivity.java
@@ -1,6 +1,7 @@
 package yanasmirnova.com.blogreader;
 
 import android.app.ListActivity;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 
@@ -20,20 +21,32 @@ public class MainListActivity extends ListActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_list);
 
-        try {
-            URL blogFeedUrl = new URL("http://blog.teamtreehouse.com/api/get_recent_summary/?count=" + NUMBER_OF_POSTS);
-            HttpURLConnection connection = (HttpURLConnection) blogFeedUrl.openConnection();
-            connection.connect();
+        GetBlogPostsTask getBlogPostsTask = new GetBlogPostsTask();
+        getBlogPostsTask.execute();
+    }
 
-            int responseCode = connection.getResponseCode();
-            Log.i(TAG, "Code: " + responseCode);
-        } catch (MalformedURLException e) {
-            Log.e(TAG, "Exception caught: ", e);
-        } catch (IOException e) {
-            Log.e(TAG, "Exception caught: ", e);
-        }
-        catch (Exception e) {
-            Log.e(TAG, "Exception caught: ", e);
+    private class GetBlogPostsTask extends AsyncTask<Object, Void, String> {
+
+        @Override
+        protected String doInBackground(Object[] params) {
+            int responseCode = -1;
+
+            try {
+                URL blogFeedUrl = new URL("http://blog.teamtreehouse.com/api/get_recent_summary/?count=" + NUMBER_OF_POSTS);
+                HttpURLConnection connection = (HttpURLConnection) blogFeedUrl.openConnection();
+                connection.connect();
+                responseCode = connection.getResponseCode();
+                Log.i(TAG, "Code: " + responseCode);
+            } catch (MalformedURLException e) {
+                Log.e(TAG, "Exception caught: ", e);
+            } catch (IOException e) {
+                Log.e(TAG, "Exception caught: ", e);
+            }
+            catch (Exception e) {
+                Log.e(TAG, "Exception caught: ", e);
+            }
+
+            return "Code: " + responseCode;
         }
     }
 
