55457606f42895e080a61ba0c4a295f266b0458b
YanaSmirnova <smirnova.yana@gmail.com>
2015-02-10 14:36:46 +1100

    Set HttpURLConnection and catching the exceptions

--- app/src/main/java/yanasmirnova/com/blogreader/MainListActivity.java
+++ app/src/main/java/yanasmirnova/com/blogreader/MainListActivity.java
@@ -1,24 +1,40 @@
 package yanasmirnova.com.blogreader;
 
 import android.app.ListActivity;
-import android.content.res.Resources;
 import android.os.Bundle;
-import android.widget.ArrayAdapter;
+import android.util.Log;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class MainListActivity extends ListActivity {
 
-    protected String[] mAndroidNames;
+    protected String[] mBlogPostTitles;
+    public static final int NUMBER_OF_POSTS = 20;
+    public static final String TAG = MainListActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_list);
 
-        Resources resources = getResources();
-        mAndroidNames = resources.getStringArray(R.array.android_names);
+        try {
+            URL blogFeedUrl = new URL("http://blog.teamtreehouse.com/api/get_recent_summary/?count=" + NUMBER_OF_POSTS);
+            HttpURLConnection connection = (HttpURLConnection) blogFeedUrl.openConnection();
+            connection.connect();
 
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, mAndroidNames);
-        setListAdapter(adapter);
+            int responseCode = connection.getResponseCode();
+            Log.i(TAG, "Code: " + responseCode);
+        } catch (MalformedURLException e) {
+            Log.e(TAG, "Exception caught: ", e);
+        } catch (IOException e) {
+            Log.e(TAG, "Exception caught: ", e);
+        }
+        catch (Exception e) {
+            Log.e(TAG, "Exception caught: ", e);
+        }
     }
 
 }
